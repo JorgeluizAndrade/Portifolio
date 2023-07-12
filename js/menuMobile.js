e01433a09d05a10b53fb63320f551048cd4e77f0
jojo <luizj1718@gmail.com>
2023-07-12 11:02:35 -0300

    corrigindo bug no menu mobile

--- js/menuMobile.js
+++ js/menuMobile.js
@@ -1,9 +1,33 @@
-function toggleMobileMenu() {
-  const mobileMenuList = document.querySelector('.header__menu-mobile-list');
-  mobileMenuList.classList.toggle('active');
-}
+// function toggleMobileMenu() {
+//   const mobileMenuList = document.querySelector('.header__menu-mobile-list');
+//   mobileMenuList.classList.toggle('active');
+// }
 
-function closeMobileMenu() {
-  const mobileMenuList = document.querySelector('.header__menu-mobile-list');
-  mobileMenuList.classList.remove('active');
-}
+// function closeMobileMenu() {
+//   const mobileMenuList = document.querySelector('.header__menu-mobile-list');
+//   mobileMenuList.classList.remove('active');
+// }
+
+
+const navSlide = () => {
+  const burger = document.querySelector(".header__menu-mobile");
+  const nav = document.querySelector(".header__menu-mobile-list");
+  const navLinks = document.querySelectorAll(".header__nav a");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("header__nav-active");
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s `;
+      }
+    });
+  });
+};
+
+navSlide();
+
