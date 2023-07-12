b7124ebb2a98e9e4c98e821bf1d875109817eded
jojo <luizj1718@gmail.com>
2023-07-12 09:47:33 -0300

    colocando type defer no js

--- js/menuMobile.js
+++ js/menuMobile.js
@@ -1,10 +1,9 @@
 function toggleMobileMenu() {
-    const mobileMenuList = document.querySelector('.header__menu-mobile-list');
-    mobileMenuList.classList.toggle('active');
-  }
-  
-  function closeMobileMenu() {
-    const mobileMenuList = document.querySelector('.header__menu-mobile-list');
-    mobileMenuList.classList.remove('active');
-  }
-  
+  const mobileMenuList = document.querySelector('.header__menu-mobile-list');
+  mobileMenuList.classList.toggle('active');
+}
+
+function closeMobileMenu() {
+  const mobileMenuList = document.querySelector('.header__menu-mobile-list');
+  mobileMenuList.classList.remove('active');
+}
